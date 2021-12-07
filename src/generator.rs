51e995839262aa49da99f724ca98c6e4b5f71d34
alvise <alvisebruniera@gmail.com>
2021-12-07 18:02:09 +0100

    Added support for chain_length and device_length

--- src/generator.rs
+++ src/generator.rs
@@ -51,25 +51,84 @@ impl Config {
             coordinator_image: "abulang/abusim-coordinator:latest".to_string(),
             namespace: "abusim-example".to_string(),
             includes: vec![],
-            agents: generate_devices(opt.devices_number),
-            prototypes: HashMap::new(),
+            agents: generate_devices(&opt),
+            prototypes: generate_prototypes(&opt),
         }
     }
 }
 
-fn generate_devices(devices_number: u32) -> HashMap<String, Agent> {
-    (0..devices_number)
+fn generate_devices(opt: &Opt) -> HashMap<String, Agent> {
+    (0..(opt.devices_number))
         .map(|id| {
             (
                 id.to_string(),
                 Agent {
                     prototype: "agent".to_string(),
                     memory_controller: "basic".to_string(),
-                    memory: vec![String::from(format!("integer:id:{}", id))],
-                    rules: vec![],
+                    memory: vec![
+                        String::from(format!("integer:id:{}", id)),
+                        "bool:start:false".to_string(),
+                    ],
+                    rules: vec![
+                        String::from(format!(
+                            "rule start on start for start do a0 = {}; start = false",
+                            opt.devices_length
+                        )),
+                        String::from(format!(
+                            "rule activate on a{0} \
+                                for all this.a{0} > 0 && (\
+                                    ext.id == (this.id + 1) || (\
+                                        this.id == {1} && ext.id == 0\
+                                    )\
+                                ) \
+                                do ext.a0 = (this.a{0} - 1)",
+                            opt.chain_length - 1,
+                            opt.devices_number - 1
+                        )),
+                    ],
                     tick: "1s".to_string(),
                 },
             )
         })
         .collect()
+}
+
+fn generate_prototypes(opt: &Opt) -> HashMap<String, Prototype> {
+    let mut prototypes = HashMap::new();
+    let transform = get_rules_generator(&opt);
+
+    prototypes.insert(
+        "agent".to_string(),
+        Prototype {
+            memory_controller: "basic".to_string(),
+            memory: (0..(opt.chain_length))
+                .map(|index| String::from(format!("integer:a{}:0", index)))
+                .collect(),
+            rules: (0..(opt.chain_length)).map(transform).collect(),
+            tick: "1s".to_string(),
+        },
+    );
+
+    prototypes
+}
+
+fn get_rules_generator(opt: &Opt) -> impl Fn(u32) -> String + '_ {
+    |index: u32| {
+        if index == opt.chain_length - 1 {
+            String::from(format!(
+                "rule last_step on a{0} \
+                    for a{0} > 0 \
+                    do a{0} = 0",
+                index
+            ))
+        } else {
+            String::from(format!(
+                "rule step{0} on a{0} \
+                    for a{0} > 0 \
+                    do a{1} = a{0}; a{0} = 0",
+                index,
+                index + 1
+            ))
+        }
+    }
 }
