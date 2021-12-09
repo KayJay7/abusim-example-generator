fe686413a0d629e6a06394073826f9deb693e03b
alvise <alvisebruniera@gmail.com>
2021-12-09 16:25:07 +0100

    Added support for chains_number

--- src/generator.rs
+++ src/generator.rs
@@ -67,7 +67,7 @@ fn generate_devices(opt: &Opt) -> HashMap<String, Agent> {
     (0..(opt.devices_number))
         .map(|id| {
             (
-                id.to_string(),
+                String::from(format!("agent{}", id)),
                 Agent {
                     prototype: "agent".to_string(),
                     memory_controller: "basic".to_string(),
@@ -82,41 +82,13 @@ fn generate_devices(opt: &Opt) -> HashMap<String, Agent> {
 
 fn generate_prototypes(opt: &Opt) -> HashMap<String, Prototype> {
     let mut prototypes = HashMap::new();
-    let transform = get_rules_generator(&opt);
 
     prototypes.insert(
         "agent".to_string(),
         Prototype {
             memory_controller: "basic".to_string(),
-            memory: (0..(opt.chain_length))
-                .map(move |step_index| String::from(format!("integer:a{}:0", step_index)))
-                .chain([
-                    "bool:start:false".to_string(),
-                    "bool:start_all:false".to_string(),
-                ])
-                .collect(),
-            rules: (0..(opt.chain_length))
-                .map(transform)
-                .chain([
-                    String::from(format!(
-                        "rule start on start for start do a0 = {}; start = false",
-                        opt.devices_length
-                    )),
-                    "rule start_all on start_all for all this.start_all do ext.start = true".to_string(),
-                    "rule start_local on start_all for start_all do start = true; start_all = false".to_string(),
-                    String::from(format!(
-                        "rule activate on a{0} \
-                                for all this.a{0} > 0 && (\
-                                    ext.id == (this.id + 1) || (\
-                                        this.id == {1} && ext.id == 0\
-                                    )\
-                                ) \
-                                do ext.a0 = (this.a{0} - 1)",
-                        opt.chain_length - 1,
-                        opt.devices_number - 1
-                    )),
-                ])
-                .collect(),
+            memory: generate_memory(&opt),
+            rules: generate_rules(&opt),
             tick: "1s".to_string(),
         },
     );
@@ -124,24 +96,75 @@ fn generate_prototypes(opt: &Opt) -> HashMap<String, Prototype> {
     prototypes
 }
 
-/// Returns a closure that maps the rule's index to a rule
-fn get_rules_generator(opt: &Opt) -> impl Fn(u32) -> String + '_ {
-    |index: u32| {
-        if index == opt.chain_length - 1 {
-            String::from(format!(
-                "rule last_step on a{0} \
-                    for a{0} > 0 \
-                    do a{0} = 0",
-                index
-            ))
-        } else {
-            String::from(format!(
-                "rule step{0} on a{0} \
-                    for a{0} > 0 \
-                    do a{1} = a{0}; a{0} = 0",
-                index,
-                index + 1
-            ))
+/// Maps the rule's indexes to a rule
+fn get_rule(opt: &Opt, chain_index: u32, step_index: u32) -> String {
+    if step_index == opt.chain_length - 1 {
+        String::from(format!(
+            "rule last_step{0} on a{0}_{1} \
+                    for a{0}_{1} > 0 \
+                    do a{0}_{1} = 0",
+            chain_index, step_index
+        ))
+    } else {
+        String::from(format!(
+            "rule step{0}_{1} on a{0}_{1} \
+                    for a{0}_{1} > 0 \
+                    do a{0}_{2} = a{0}_{1}; a{0}_{1} = 0",
+            chain_index,
+            step_index,
+            step_index + 1
+        ))
+    }
+}
+
+fn generate_memory(opt: &Opt) -> Vec<String> {
+    let mut memory =
+        Vec::with_capacity((opt.chain_length as usize * opt.chains_number as usize) + 2);
+    memory.push("bool:start:false".to_string());
+    memory.push("bool:start_all:false".to_string());
+
+    for chain_index in 0..opt.chains_number {
+        for step_index in 0..opt.chain_length {
+            memory.push(String::from(format!(
+                "integer:a{}_{}:0",
+                chain_index, step_index
+            )));
         }
     }
+
+    memory
+}
+
+fn generate_rules(opt: &Opt) -> Vec<String> {
+    let mut rules =
+        Vec::with_capacity((opt.chain_length as usize * opt.chains_number as usize) + 4);
+
+    rules.push(String::from(format!(
+        "rule start on start for start do a0_0 = {}; start = false",
+        opt.devices_length
+    )));
+    rules
+        .push("rule start_all on start_all for all this.start_all do ext.start = true".to_string());
+    rules.push(
+        "rule start_local on start_all for start_all do start = true; start_all = false"
+            .to_string(),
+    );
+    rules.push(String::from(format!(
+        "rule activate on a0_{0} \
+            for all this.a0_{0} > 0 && (\
+                ext.id == (this.id + 1) || (\
+                    this.id == {1} && ext.id == 0\
+                )\
+            ) \
+            do ext.a0_0 = (this.a0_{0} - 1)",
+        opt.chain_length - 1,
+        opt.devices_number - 1
+    )));
+
+    for chain_index in 0..opt.chains_number {
+        for step_index in 0..opt.chain_length {
+            rules.push(get_rule(&opt, chain_index, step_index));
+        }
+    }
+    rules
 }
