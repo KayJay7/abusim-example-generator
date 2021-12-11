877b78dbcc49390c7d494de146eed1e0a4364ae2
alvise <alvisebruniera@gmail.com>
2021-12-11 19:54:51 +0100

    Added argument validation

--- src/args.rs
+++ src/args.rs
@@ -33,3 +33,43 @@ pub struct Opt {
     #[clap(short = 'f', long, default_value = "1")]
     pub devices_length: u32,
 }
+
+impl Opt {
+    pub fn is_valid(&self) -> bool {
+        let mut valid = true;
+        if self.devices_number == 0 {
+            eprintln!("Invalid argument: --devices-number must be at least 1");
+            valid = false;
+        }
+
+        if self.chains_number == 0 {
+            eprintln!("Invalid argument: --chains-number must be at least 1");
+            valid = false;
+        }
+
+        if self.chain_length == 0 {
+            eprintln!("Invalid argument: --chain-length must be at least 1");
+            valid = false;
+        }
+
+        if self.chain_width == 0 || self.chain_width > self.chains_number {
+            eprintln!(
+                "Invalid argument: --chain-width must be in between 1 and --chains-number(={})",
+                self.chains_number
+            );
+            valid = false;
+        }
+
+        if self.devices_width == 0 {
+            eprintln!("Invalid argument: --devices-width must be at least 1");
+            valid = false;
+        }
+
+        if self.devices_length == 0 {
+            eprintln!("Invalid argument: --devices-length must be at least 1");
+            valid = false;
+        }
+
+        valid
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,18 @@ fn main() {
     let opt = Opt::parse();
     #[cfg(debug_assertions)]
     println!("{:#?}", opt);
-    let file = File::create(&opt.output);
-    match file {
-        Ok(file) => {
-            let config = Config::from(opt);
-            serde_yaml::to_writer(file, &config).unwrap();
-            #[cfg(debug_assertions)]
-            println!("{}", serde_yaml::to_string(&config).unwrap());
-        }
-        Err(code) => {
-            eprintln!("Unable to create file: {}", code);
+    if opt.is_valid() {
+        let file = File::create(&opt.output);
+        match file {
+            Ok(file) => {
+                let config = Config::from(opt);
+                serde_yaml::to_writer(file, &config).unwrap();
+                #[cfg(debug_assertions)]
+                println!("{}", serde_yaml::to_string(&config).unwrap());
+            }
+            Err(code) => {
+                eprintln!("Unable to create file: {}", code);
+            }
         }
     }
 }
